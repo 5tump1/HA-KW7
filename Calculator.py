34d2a680a8384ce687cf8c27ada3c61f9a03dfea
5tump1 <c.stumpe@arcor.de>
2023-02-13 14:31:59 +0100

    If eingebaut, print ergebnis

--- Calculator.py
+++ Calculator.py
@@ -3,3 +3,18 @@
 #Benutzereingabe
 zahl1 = int(input("erste Zahl: "))
 zahl2 = int(input("zweite Zahl: "))
+operator = input("Addieren (+), Subtrahieren (-), Multiplizieren (*), Dividieren (/) : " )
+
+ergebnis=0
+
+if operator == "+":
+    ergebnis = zahl1+zahl2
+if operator == "-":
+    ergebnis = zahl1-zahl2
+if operator == "*":
+    ergebnis = zahl1*zahl2
+if operator == "/":
+    ergebnis = zahl1/zahl2
+
+#Ergebnis anzeigen:
+print(f"{zahl1} {operator} {zahl2} = ", (ergebnis))
