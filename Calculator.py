50bde903cb44e6218fd5e6e862823aa4d03d918c
5tump1 <c.stumpe@arcor.de>
2023-02-13 14:33:00 +0100

    Kommentare hinzugefügt

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,15 @@
 # Calculator
 
 #Benutzereingabe
+#erste zahl eingeben
 zahl1 = int(input("erste Zahl: "))
+#zweite zahl eingeben
 zahl2 = int(input("zweite Zahl: "))
+#welche rechenoperation soll durchgeführt werden
 operator = input("Addieren (+), Subtrahieren (-), Multiplizieren (*), Dividieren (/) : " )
-
-ergebnis=0
-
+#ergebnis auf null setzen
+ergebnis = 0
+#Bedingungen eingebaut
 if operator == "+":
     ergebnis = zahl1+zahl2
 if operator == "-":
