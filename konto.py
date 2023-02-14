a6602a69c747a02518011000dcc832d76a808867
5tump1 <c.stumpe@arcor.de>
2023-02-14 14:29:36 +0100

    funktion abfragen für besitzer und kontostand eingefügt

--- konto.py
+++ konto.py
@@ -1,11 +1,15 @@
 class bankkonto:
-    def __init__(self, eigentuemer, kontostand):
-        self.eigentuemer = eigentuemer
+    def __init__(self, besitzer, kontostand):
+        self.besitzer = besitzer
         self.kontostand = kontostand
+    def abfrage_kontostand(self):
+        print(f"Der Kontostand beträgt {self.kontostand}")
+    def abfrage_besitzer(self):
+        print(f"Der Besitzer des kontos ist {self.besitzer}")
 
 
 chris = bankkonto("Chris", 100.00)
 padde = bankkonto("Padde", 245.89)
 
-print(f"{chris.eigentuemer}: {chris.kontostand}")
-print(f"{padde.eigentuemer}: {padde.kontostand}")
+print(f"{chris.besitzer}: {chris.kontostand}")
+print(f"{padde.besitzer}: {padde.kontostand}")
