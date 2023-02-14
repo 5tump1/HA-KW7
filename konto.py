c1856d21fe6b3957bce506bf160dcd5af8b88951
5tump1 <c.stumpe@arcor.de>
2023-02-14 14:37:44 +0100

    def str eingefügt

--- konto.py
+++ konto.py
@@ -2,6 +2,8 @@ class bankkonto:
     def __init__(self, besitzer, kontostand):
         self.besitzer = besitzer
         self.kontostand = kontostand
+    def __str__(self):
+        return f"Dieses Bankkonto gehört {self.besitzer} und hat ein guthaben von {self.kontostand}"
     def abfrage_kontostand(self):
         print(f"Der Kontostand beträgt {self.kontostand}")
     def abfrage_besitzer(self):
@@ -13,3 +15,6 @@ padde = bankkonto("Padde", 245.89)
 
 print(f"{chris.besitzer}: {chris.kontostand}")
 print(f"{padde.besitzer}: {padde.kontostand}")
+
+
+
