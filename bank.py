d44bae4f411838190364adfda79a23c7477ce27b
5tump1 <c.stumpe@arcor.de>
2023-02-15 15:40:25 +0100

    bank bearbeitet, klassen angelegt

--- bank.py
+++ bank.py
@@ -3,4 +3,15 @@
 class bank:
     pass
 
-def
+def __str__(self):
+    return f"Dieses Konto gehört {self.besitzer} und hat ein Guthaben von {self.kontostand}"
+
+
+class konto:
+    pass
+
+class konto:
+    pass
+
+class kredit:
+    pass
