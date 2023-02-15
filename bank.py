c56ba8635e604ad836527aa8c1f65677b5e1c2aa
5tump1 <c.stumpe@arcor.de>
2023-02-15 16:17:04 +0100

    WIP Kredite... ich hänge

--- bank.py
+++ bank.py
@@ -19,6 +19,10 @@ class bankkonto:
             print("Sorry, aber du hast nicht genug geld!")
         else:
             self.kontostand = self.kontostand - betrag
+class kindersparkonto(bankkonto):
+    def __init__(self, besitzer, kind, kontostand):
+        super().__init__(besitzer, kontostand)
+        self.kind = kind
 
 konten = {
     "chris": bankkonto("Christopher", 250.00),
@@ -30,22 +34,33 @@ print(konten["chris"])
 print(konten["padde"])
 print(konten["uli"])
 
-class kinderkonto:
-    def __init__(self, besitzer, kind, kontostand):
-        super().__init__(besitzer, kontostand)
-        self.kind = kind
+str_konto = input("Welches Konto möchten Sie bearbeiten [Chris, Padde, Uli]? ").lower()
+str_aktion = input("Möchten sie ein- oder auszahlen [Einzahlen, Auszahlen]? ").lower()
 
+str_betrag = input(f"Welchen betrag möchten sie {str_aktion}? ")
+
+if str_aktion.lower() == "einzahlen":
+    konten[str_konto].einzahlen(float(str_betrag))
+elif str_aktion == "auszahlen":
+    konten[str_konto].auszahlen(float(str_betrag))
+else:
+    print("Konnte Aktion \"{str_aktion}\" nicht verstehen")
+print(f"Neuer Kontostand von {konten[str_konto].besitzer} beträgt "
+      f"{konten[str_konto].kontostand}")
+
+""""
 class kredit:
-    def __init__(self, besitzer, volumen):
-        super().__init__(besitzer)
-        self.volumen = kreditvolumen
+    def __init__(self, kreditvolumen):
+        super().__init__(besitzer, kreditvolumen)
+        self.kreditvolumen = kreditvolumen
 
-volumen = {
-    "chris" : kredit("Christopher", 10000.00),
+kreditvolumen = {
+    "chris" : kredit("Christopher", 10000.0),
     "padde" : kredit("Patrick", 150000.45),
     "uli" : kredit("Uli", 12000.33),
 }
 
 print(kredit["chris"])
 print(kredit["padde"])
-print(kreidt["uli"])
+print(kredit["uli"])
+"""
